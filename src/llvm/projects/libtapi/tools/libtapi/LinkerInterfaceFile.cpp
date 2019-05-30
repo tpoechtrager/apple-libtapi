e92ebe1484033f91c80d9cf1a90c44f95c8c81fa
Thomas Pöchtrager <t.poechtrager@gmail.com>
2019-05-30 18:34:46 +0200

    An attempt to get TAPI platform "zippered" working #2

--- src/llvm/projects/libtapi/tools/libtapi/LinkerInterfaceFile.cpp
+++ src/llvm/projects/libtapi/tools/libtapi/LinkerInterfaceFile.cpp
@@ -298,6 +298,10 @@ static tapi::Platform mapPlatform(tapi::internal::Platform platform) {
     return Platform::tvOS;
   case tapi::internal::Platform::bridgeOS:
     return Platform::bridgeOS;
+  case tapi::internal::Platform::iOSMac:
+    return Platform::iOSMac;
+  case tapi::internal::Platform::zippered:
+    return Platform::zippered;
   }
 }
 
