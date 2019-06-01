f3905245337663894e7587515817331287a5378a
Thomas Pöchtrager <t.poechtrager@gmail.com>
2019-06-01 13:05:27 +0200

    Remove header installation quirks

--- install.sh
+++ install.sh
@@ -8,12 +8,7 @@ pushd build &>/dev/null
 
 INSTALLPREFIX=$(cat INSTALLPREFIX)
 
-mkdir -p $INSTALLPREFIX
-mkdir -p $INSTALLPREFIX/include
-cp -rva ../src/llvm/projects/libtapi/include/tapi $INSTALLPREFIX/include
-cp -va projects/libtapi/include/tapi/Version.inc $INSTALLPREFIX/include/tapi
-
-$MAKE install-libtapi -j $JOBS
+$MAKE install-libtapi install-tapi-headers -j $JOBS
 
 popd &>/dev/null
 popd &>/dev/null
