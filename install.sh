e9720db247787d6f02a24a7afd02041c1d078b13
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-11-12 21:52:15 -0500

    fix

--- install.sh
+++ install.sh
@@ -15,7 +15,13 @@ INSTALLTARGETS="install-libtapi install-tapi-headers"
 [ -e "bin/tapi-run" ] && INSTALLTARGETS+=" install-tapi-run"
 [ -e "bin/tapi-sdkdb" ] && INSTALLTARGETS+=" install-tapi-sdkdb"
 
-[ -f build.ninja ] && command -v ninja &>/dev/null && MAKE=ninja
+if [ -f build.ninja ]; then
+    command -v ninja &>/dev/null || {
+        echo "Missing ninja" 1>&2
+        exit 1
+    }
+    MAKE=ninja
+fi
 
 $MAKE $INSTALLTARGETS -j $JOBS
 
