640b4623929c923c0468143ff2a363a48665fa54
Thomas Pöchtrager <t.poechtrager@gmail.com>
2025-05-02 22:26:42 +0200

    Another header file where stdint.h include is missing

--- src/tapi/include/tapi/tapi.h
+++ src/tapi/include/tapi/tapi.h
@@ -21,6 +21,7 @@
 /// The C++ Application Programming Interface (API) for the TAPI library
 ///
 
+#include <stdint.h>
 #include <tapi/APIVersion.h>
 #include <tapi/Defines.h>
 #include <tapi/LinkerInterfaceFile.h>
